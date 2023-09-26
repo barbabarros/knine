ee19bafd3d4b6679b8553c0dbcf37b02aa451eef
barbabarros <barbabarros@windowslive.com>
2023-09-26 18:15:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1 class="code-line" data-line-start=1 data-line-end=2 ><a id="knine_1"></a>knine</h1>
+<p class="has-line-data" data-line-start="2" data-line-end="3">Um simples programa para verificar se uma URL contem arquivos pdf.</p>
+<h1 class="code-line" data-line-start=4 data-line-end=5 ><a id="Uso_4"></a>Uso</h1>
+<p class="has-line-data" data-line-start="6" data-line-end="9">url_base = &quot;<a href="https://example.com/">https://example.com/</a>&quot;   # URL destino com / no final. O nome do arquivo será a variável inicio + .pdf<br>
+inicio = 1   # Início do intervalo contendo o nome do arquivo .pdf<br>
+fim = 10   # Fim do intervalo contendo o nome do arquivo .pdf</p>
+<p class="has-line-data" data-line-start="10" data-line-end="12">Neste exemplo, serão testadas todas URLs com 18 dígitos, iniciando em <a href="https://example.com/000000000000000001.pdf">https://example.com/000000000000000001.pdf</a> e terminando em <a href="https://example.com/000000000000000010.pdf">https://example.com/000000000000000010.pdf</a>.<br>
+Para alterar a quantidade de digitos, verifique a linha 25.</p>
+<p class="has-line-data" data-line-start="13" data-line-end="14">As URLs válidas serão salvas no arquivo especificado.</p>
---
+++ knine.py
@@ -0,0 +1,39 @@
+import requests
+import mimetypes
+import time
+
+url_base = "https://example.com/"   # URL destino com / no final. O nome do arquivo será a variável inicio + .pdf 
+inicio = 1   # Início do intervalo contendo o nome do arquivo .pdf
+fim = 2   # Fim do intervalo contendo o nome do arquivo .pdf
+delay = 1.3   # Intervalo entre as requisições (1.3 = 1 segundo e 300 milissegundos) 
+arquivo_txt = "URLs.txt"   # Nome do arquivo para salvar as URLs válidas. Será salvo no mesmo diretório de execução do programa
+
+def valida_url_pdf(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            content_type = response.headers.get('content-type')
+            return mimetypes.guess_extension(content_type) == '.pdf'
+        return False
+    except requests.exceptions.RequestException as e:
+        print(f"Erro ao acessar URL {url}: {e}")
+        return False
+
+def gera_e_salva_as_urls_validadas():
+    urls_validas = []
+    for i in range(inicio, fim + 1):  # Itera dentro do intervalo especificado
+        url = f"{url_base}{i:018}.pdf"  # Formata o número com 18 dígitos
+        print(f"Verificando URL: {url}")
+        if valida_url_pdf(url):
+            urls_validas.append(url)
+            print(f"URL válida encontrada: {url}")
+        
+        # Adiciona um atraso de 300 ms entre as requisições
+        time.sleep(delay)
+
+    # Salva os URLs válidos em um arquivo .txt
+    with open("arquivo_txt", "w") as file:
+        for url in urls_validas:
+            file.write(url + "\n")
+
+gera_e_salva_as_urls_validadas()
